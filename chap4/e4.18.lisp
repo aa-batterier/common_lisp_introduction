14e6ecf343dc1693ca9e60235e4bb0d95b898329
Andreas Johansson <aj854@outlook.com>
2018-07-02 18:51:09 +0000

    Added exercise 18.

--- chap4/e4.18.lisp
+++ chap4/e4.18.lisp
@@ -1,4 +1,22 @@
 ; Define function PLAY which starts a game of rock, scissors, paper.
 (defun play (p1 p2)
-  (cond ((equal p1 p2) 'tie)
-	((and (equal 'rock p1) (equal 'scissors p2))
+  (cond
+    ; Player one wins.
+    ((or
+       (and (equal 'rock p1) (equal 'scissors p2))
+       (and (equal 'scissors p1) (equal 'paper p2))
+       (and (equal 'paper p1) (equal 'rock p2)))
+     'first-wins)
+    ; Player two wins.
+    ((or
+       (and (equal 'rock p2) (equal 'scissors p1))
+       (and (equal 'scissors p2) (equal 'paper p1))
+       (and (equal 'paper p2) (equal 'rock p1)))
+     'second-wins)
+    ; Tie.
+    ((or
+       (and (equal 'rock p1) (equal 'rock p2))
+       (and (equal 'scissors p1) (equal 'sicssors p2))
+       (and (equal 'paper p1) (equal 'paper p2)))
+     'tie)
+    (t 'wrong-input)))
